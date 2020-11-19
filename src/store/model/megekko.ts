0b97607f85e06b63faf88d420d251ba7cacb9d3a
Choka-tech <74742111+Choka-tech@users.noreply.github.com>
2020-11-20 00:49:16 +0100

    Create megekko.ts
    New store from megekko.nl

---
+++ src/store/model/megekko.ts
@@ -0,0 +1,432 @@
+import {Store} from './store';
+
+export const Megekko: Store = {
+	labels: {
+		inStock: {
+			container: '.subtitle',
+			text: ['1 dag', '3 dagen']
+		},
+		maxPrice: {
+			container: 'pricecontainer',
+			euroFormat: true
+		},
+		outOfStock: {
+			container: '.subtitle',
+			text: ['minimaal 10 dagen', 'pre-order']
+		}
+	},
+	links: [
+		{
+			brand: 'test:brand',
+			model: 'test:model',
+			series: 'test:series',
+			url: 'https://www.megekko.nl/product/1944/975380/DVD-ReWriter-Intern-SATA/Asus-DRW-24D5MT-OEM'
+		},
+		{
+			brand: 'asus',
+			model: 'dual',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/294403/Nvidia-Videokaarten/Asus-GeForce-RTX-3070-DUAL-RTX3070-8G-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'dual oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/294404/Nvidia-Videokaarten/Asus-GeForce-RTX-3070-DUAL-RTX3070-O8G-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118232/Nvidia-Videokaarten/Asus-Geforce-RTX-3080-TUF-RTX3080-10G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118234/Nvidia-Videokaarten/Asus-Geforce-RTX-3090-TUF-RTX3090-24G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/294405/Nvidia-Videokaarten/Asus-GeForce-RTX-3070-TUF-RTX3070-O8G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118233/Nvidia-Videokaarten/Asus-Geforce-RTX-3080-TUF-RTX3080-O10G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'tuf oc',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1119244/Nvidia-Videokaarten/Asus-Geforce-RTX-3090-TUF-RTX3090-O24G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1124775/Nvidia-Videokaarten/ASUS-ROG-NVIDIA-GeForce-RTX-3070-8GB-GDDR6-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1119242/Nvidia-Videokaarten/Asus-Geforce-RTX-3080-ROG-STRIX-RTX3080-10G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'strix',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1119247/Nvidia-Videokaarten/ASUS-Geforce-RTX-3090-ROG-STRIX-RTX3090-24G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/294645/Nvidia-Videokaarten/Asus-GeForce-RTX-3070-ROG-STRIX-RTX3070-O8G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1119243/Nvidia-Videokaarten/Asus-Geforce-RTX-3080-ROG-STRIX-RTX3080-O10G-GAMING-Videokaart'
+		},
+		{
+			brand: 'asus',
+			model: 'strix oc',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1119245/Nvidia-Videokaarten/ASUS-Geforce-RTX-3090-ROG-STRIX-RTX3090-O24G-GAMING-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 2x',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/295124/Nvidia-Videokaarten/MSI-GeForce-RTX-3070-VENTUS-2X-OC-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/295125/Nvidia-Videokaarten/MSI-GeForce-RTX-3070-VENTUS-3X-OC-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/292742/Nvidia-Videokaarten/MSI-GeForce-RTX-3080-VENTUS-3X-10G-OC-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'ventus 3x',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/292747/Nvidia-Videokaarten/MSI-GeForce-RTX-3090-VENTUS-3X-24G-OC-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/295123/Nvidia-Videokaarten/MSI-GeForce-RTX-3070-GAMING-X-TRIO-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/292740/Nvidia-Videokaarten/MSI-GeForce-RTX-3080-GAMING-X-TRIO-10G-Videokaart'
+		},
+		{
+			brand: 'msi',
+			model: 'gaming x trio',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/292745/Nvidia-Videokaarten/MSI-GeForce-RTX-3090-GAMING-X-TRIO-24G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'turbo',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/293963/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-TURBO-24G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/293969/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3070-EAGLE-8G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1125074/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3080-EAGLE-10G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1125075/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-EAGLE-24G-NVIDIA-24-GB-GDDR6X-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/293968/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3070-EAGLE-OC-8G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/292735/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3080-EAGLE-OC-10G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'eagle oc',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/292737/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-EAGLE-OC-24G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/293967/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3070-GAMING-OC-8G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/292736/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3080-GAMING-OC-10G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'gaming oc',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/292738/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-GAMING-OC-24G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/295478/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3070-VISION-OC-8G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/293966/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3080-VISION-OC-10G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'vision oc',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/295482/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-VISION-OC-24G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/295477/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3070-AORUS-MASTER-8G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/293965/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3080-AORUS-M-10G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus master',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/293962/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-AORUS-M-24G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/293964/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3080-AORUS-X-10G-Videokaart'
+		},
+		{
+			brand: 'gigabyte',
+			model: 'aorus xtreme',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/293961/Nvidia-Videokaarten/Gigabyte-GeForce-RTX-3090-AORUS-X-24G-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1122299/Nvidia-Videokaarten/EVGA-GeForce-RTX-3070-XC3-BLACK-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118246/Nvidia-Videokaarten/EVGA-GeForce-RTX-3080-XC3-BLACK-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 black',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118251/Nvidia-Videokaarten/EVGA-GeForce-RTX-3090-XC3-BLACK-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1122300/Nvidia-Videokaarten/EVGA-GeForce-RTX-3070-XC3-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118247/Nvidia-Videokaarten/EVGA-GeForce-RTX-3080-XC3-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118250/Nvidia-Videokaarten/EVGA-GeForce-RTX-3090-XC3-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118260/Nvidia-Videokaarten/EVGA-GeForce-RTX-3080-XC3-ULTRA-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'xc3 ultra',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118252/Nvidia-Videokaarten/EVGA-GeForce-RTX-3090-XC3-ULTRA-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1122301/Nvidia-Videokaarten/EVGA-GeForce-RTX-3070-FTW3-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118248/Nvidia-Videokaarten/EVGA-GeForce-RTX-3080-FTW3-GAMING-Videokaart'
+		},	
+		{
+			brand: 'evga',
+			model: 'ftw3',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118254/Nvidia-Videokaarten/EVGA-GeForce-RTX-3090-FTW3-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1122302/Nvidia-Videokaarten/EVGA-GeForce-RTX-3070-FTW3-ULTRA-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118249/Nvidia-Videokaarten/EVGA-GeForce-RTX-3080-FTW3-ULTRA-GAMING-Videokaart'
+		},
+		{
+			brand: 'evga',
+			model: 'ftw3 ultra',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118256/Nvidia-Videokaarten/EVGA-GeForce-RTX-3090-FTW3-ULTRA-GAMING-Videokaart'
+		},
+		{
+			brand: 'inno3d',
+			model: 'gaming x3',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118239/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3090-Gaming-X3-Videokaart'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1122298/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3070-Twin-X2-Videokaart'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2 oc',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1118244/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3070-Twin-X2-OC-Videokaart'
+		},
+		{
+			brand: 'inno3d',
+			model: 'twin x2 oc',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118238/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3080-Twin-X2-OC-Videokaart'
+		},
+		{	brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1118242/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3070-iChill-X3-Videokaart'
+		},
+		{	brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118236/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3080-iChill-X3-Videokaart'
+		},
+		{	brand: 'inno3d',
+			model: 'ichill x3',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118240/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3090-iChill-X3-Videokaart'
+		},
+		{	brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3070',
+			url: 'https://www.megekko.nl/product/1963/1118243/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3070-iChill-X4-Videokaart'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1118237/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3080-iChill-X4-Videokaart'
+		},
+		{
+			brand: 'inno3d',
+			model: 'ichill x4',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1118241/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3090-iChill-X4-Videokaart'
+		},
+		{	
+			brand: 'inno3d',
+			model: 'ichill frostbite',
+			series: '3080',
+			url: 'https://www.megekko.nl/product/1963/1125524/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3080-iChill-Frostbite-Videokaart'
+		},
+		{	
+			brand: 'inno3d',
+			model: 'ichill frostbite',
+			series: '3090',
+			url: 'https://www.megekko.nl/product/1963/1125525/Nvidia-Videokaarten/Inno3D-GeForce-RTX-3090-iChill-Frostbite-Videokaart'
+		},
+		{	
+			brand: 'amd',
+			model: '5600x',
+			series: 'ryzen5600',
+			url: 'https://www.megekko.nl/product/4278/294824/Socket-AM4-Processoren/AMD-Ryzen-5-5600X-processor'
+		},
+		{	
+			brand: 'amd',
+			model: '5800x',
+			series: 'ryzen5800',
+			url: 'https://www.megekko.nl/product/4278/294823/Socket-AM4-Processoren/AMD-Ryzen-7-5800X-processor'
+		},
+		{	
+			brand: 'amd',
+			model: '5900x',
+			series: 'ryzen5900',
+			url: 'https://www.megekko.nl/product/4278/294822/Socket-AM4-Processoren/AMD-Ryzen-9-5900X-processor'
+		},
+		{	
+			brand: 'amd',
+			model: '5950x',
+			series: 'ryzen5950',
+			url: 'https://www.megekko.nl/product/4278/294821/Socket-AM4-Processoren/AMD-Ryzen-9-5950X-processor'
+		}
+
+	],
+	name: 'megekko'
+};
